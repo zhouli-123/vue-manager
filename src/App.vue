5f3abc60bb4ac4be76a917dd96adc1e9dab71c08
zhouli-123 <516193147@qq.com>
2020-05-11 23:10:50 +0800

    vue-manager

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <el-button type="primary">首选项按钮</el-button>
+    <el-input type="text"></el-input>
+    <el-switch
+      v-model="value"
+      active-color="#13ce66"
+      inactive-color="#ff4949">
+    </el-switch>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data (){
+    return {
+      value: true
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/plugins/element.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { Button, Input, Switch } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Switch)
